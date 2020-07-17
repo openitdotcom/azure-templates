c4d9fcfda714f7245a3db6dd71c9c5e62b80b41f
Jhon Angelo C. Virtucio <jvirtucio@openit.com>
2020-07-17 14:15:42 +0800

    Add cleanup of install log from previous installations

--- openitclient-windows/scripts/Configure_OpeniTClient.ps1
+++ openitclient-windows/scripts/Configure_OpeniTClient.ps1
@@ -8,6 +8,26 @@ Param(
     [string] $ApplicationVersion = '9.6.30'
 )
 
+if (Test-Path "$Env:Temp\openit_install.log") 
+{
+  Remove-Item "$Env:Temp\openit_install.log"
+}
+
+function Get-Program-Version {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position = 0, Mandatory=$true, ValueFromPipeline = $true)]
+        $Name
+    )
+    $app = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | 
+                Where-Object { $_.DisplayName -match $Name } | 
+                Select-Object DisplayName, DisplayVersion, InstallDate, Version
+    if ($app) {
+        return $app.DisplayVersion
+    }
+}
+
+
 $currentVersion = Get-Program-Version "Open iT Client" 
 
 if ($currentVersion == $ApplicationVersion) {
@@ -90,18 +110,3 @@ catch {
 Remove-Item "$Env:Temp\$Container" -Force -Recurse
     
 
-
-
-function Get-Program-Version {
-    [CmdletBinding()]
-    Param(
-        [Parameter(Position = 0, Mandatory=$true, ValueFromPipeline = $true)]
-        $Name
-    )
-    $app = Get-ItemProperty -Path "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*" | 
-                Where-Object { $_.DisplayName -match $Name } | 
-                Select-Object DisplayName, DisplayVersion, InstallDate, Version
-    if ($app) {
-        return $app.DisplayVersion
-    }
-}
